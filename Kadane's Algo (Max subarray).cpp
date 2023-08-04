e81ca24b23ad1733c4d4e928a652ac0ade0dcada
Abhishek Singh <81374346+Abhishek17-10@users.noreply.github.com>
2023-08-04 17:19:01 +0530

    Create Kadane's Algo (Max subarray).cpp

---
+++ Kadane's Algo (Max subarray).cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int answer=INT_MIN;
+		
+        int temp=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            temp=temp+nums[i];
+            
+            if(temp>answer)
+            {  
+                answer=temp;
+            }
+            
+            if(temp<0)
+            {
+                temp=0;
+            }
+        }
+        
+        return answer;
+    }
+};
