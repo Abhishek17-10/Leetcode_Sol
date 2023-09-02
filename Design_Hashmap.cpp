3be3ebbe570e205ef017576afa1d9a5f971ea6a1
Abhishek Singh <81374346+Abhishek17-10@users.noreply.github.com>
2023-09-02 13:14:38 +0530

    Create Design_Hashmap.cpp

---
+++ Design_Hashmap.cpp
@@ -0,0 +1,30 @@
+class MyHashMap {
+public:
+
+    unordered_map<int, int> umap;
+    MyHashMap() { 
+    }
+    
+    void put(int key, int value) {
+        umap[key]=value;
+    }
+    
+    int get(int key) {
+        if (umap.find(key) == umap.end())  
+           return -1;
+        auto it=umap.find(key);
+        return it->second;
+    }
+    
+    void remove(int key) {
+        umap.erase(key);
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
